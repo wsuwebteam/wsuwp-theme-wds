1473ca60a7d21e230083a224c346e9055c0f40d7
Dan White <dan.white1@wsu.edu>
2021-10-11 13:40:21 -0700

    Implement 404 page content

---
+++ template-parts/template-404-content.php
@@ -0,0 +1,11 @@
+<p>
+    We're sorry, but the page you are looking for could not be found. 
+    Please try our site search or one of the links below.
+</p>
+<ul>
+    <li><a href="https://index.wsu.edu">The A-Z Directory</a> (an alphabetical list of departments, programs, resources, organizations, and more)</li>
+    <li><a href="https://wsu.edu/academics/">Academic Departments</a> (a list of links to all academic departments)</li>
+    <li><a href="https://wsu.edu/about/services/">Administrative Offices and Resources</a> (a list of links to all administrative offices and resources)</li>
+    <li><a href="https://wsu.edu/admission/">Admissions and Aid</a> (information for prospective students about applying, tuition, financial aid, and more)</li>
+    <li><a href="https://wsu.edu/contact/">Contact Us</a> (phone and email contacts)</li>
+</ul>
