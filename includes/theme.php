5a4d45141edd428316b08910324f7cbb7fc1feeb
Danial Bleile <danbleile@gmail.com>
2021-11-01 11:35:43 -0700

    Enhancements | Add support for rest api AND logic

--- includes/theme.php
+++ includes/theme.php
@@ -30,6 +30,7 @@ class Theme {
 		require_once __DIR__ . '/sidebars.php';
 		require_once __DIR__ . '/supports.php';
 		require_once __DIR__ . '/wds_options.php';
+		require_once __DIR__ . '/rest-api.php';
 
 	}
 
