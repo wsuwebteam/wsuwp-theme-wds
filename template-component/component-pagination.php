41e8d1066e08af6157d409b7fc47bbf55a3eaa9b
Danial Bleile <danbleile@gmail.com>
2021-11-17 15:30:38 -0800

    Enhancement | Add pagination

---
+++ template-component/component-pagination.php
@@ -0,0 +1,4 @@
+<div class="wsu-pagination">
+	<div class="wsu-pagination__left"><?php previous_posts_link( 'Previous' ); ?></div>
+	<div class="wsu-pagination__right"><?php next_posts_link( 'Next' ); ?></div>
+</div>
