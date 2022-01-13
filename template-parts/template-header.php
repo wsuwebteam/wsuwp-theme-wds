6707e31549505877d6c577d694ea159158d0461b
Danial Bleile <danbleile@gmail.com>
2022-01-13 13:24:58 -0800

    Enhancement | Breadcrumbs | Add breadcrumb support

--- template-parts/template-header.php
+++ template-parts/template-header.php
@@ -1,6 +1,7 @@
 <?php $context = ( ! empty( $args['context'] ) ) ? $args['context'] : 'single'; ?>
 
 <?php if ( apply_filters( 'wsu_wds_template_option', true, 'show_title', $context ) ) : ?>
+<?php get_template_part( 'template-component/component-breadcrumb', $context ); ?>
 <header class="wsu-article-header">
     <h1  class="wsu-article-header__title"><?php the_title(); ?></h1>
     <?php if ( apply_filters( 'wsu_wds_template_option', true, 'show_publish_date', $context ) ) : ?><?php get_template_part( 'template-component/component-post-published-date', $context ); ?><?php endif; ?>
