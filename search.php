77cdd7e704b978ad6fb1c20088a0de7f1ec661be
Danial Bleile <danbleile@gmail.com>
2022-08-12 08:21:04 -0700

    Enhancement | Add Google Search

--- search.php
+++ search.php
@@ -1,5 +1,6 @@
 <?php namespace WSUWP\Theme\WDS; 
 
+$search_term = ( ! empty( $_REQUEST['s'] ) ) ? sanitize_text_field( $_REQUEST['s'] ) : '';
 ?>
 <?php get_header(); ?>
 <?php get_template_part( 'template-component/component-global-header', 'search' ); ?>
@@ -17,23 +18,28 @@
 			<header class="wsu-page-header">
 				<h1  class="wsu-page-header__title">Search</h1>
 			</header>
+			<section class="wsu-section  wsu-width--full wsu-spacing-after--xxmedium">
+    			<div class="wsu-section__inner wsu-width--content">    
+					<form class="wsu-search " method="get" action="<?php echo get_site_url(); ?>">
+						<div class="wsu-search__search-bar">
+							<input class="wsu-search__input" type="text" aria-lable="Search input" placeholder="Search" name="s" value="<?php echo $search_term; ?>" />
+							<button class="wsu-search__submit" aria-lable="Submit Search"></button>
+						</div>
+
+						<div class="wsu-search__search-options">
+							<input type="radio" class="wsu-search__search-toggle" id="wsu-search__search-toggle-site" name="search_context" value="site" <?php if ( empty( $_REQUEST['search_context'] ) || 'site' === $_REQUEST['search_context'] ) : ?> checked="checked"<?php endif; ?> /><label for="wsu-search__search-toggle-site" class="wsu-search__search-toggle-label"> This Site</label>
+							<input type="radio" class="wsu-search__search-toggle" id="wsu-search__search-toggle-wsu" name="search_context" value="wsu" <?php if ( 'wsu' === $_REQUEST['search_context'] ) : ?> checked="checked"<?php endif; ?> /><label for="wsu-search__search-toggle-wsu" class="wsu-search__search-toggle-label"> All WSU</label>
+						</div>
+
+					</form>
+    			</div>
+			</section>
+			<script async src="https://cse.google.com/cse.js?cx=54e8a42262bb5e5f2"></script>
+			<div class="gcse-searchresults-only" data-queryParameterName="s" <?php if ( empty( $_REQUEST['search_context'] ) || 'site' === $_REQUEST['search_context'] ) : ?>data-as_sitesearch="<?php echo get_site_url(); ?>"<?php endif; ?>></div>
 			<?php if ( ! empty( $_REQUEST['s'] ) ) : ?>
-				<?php get_search_form( array('echo' => true ) ); ?> 
-				<?php if ( have_posts() ) : ?>
-					<ul class="wsu-search-results">
-					<?php while ( have_posts() ) : the_post(); ?>
-					<li>
-						<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a> 
-						<?php the_excerpt(); ?>
-						<div class="wsu-meta-date"><time><?php the_date(); ?></time></div>
-					</li>
-					<?php endwhile; ?>
-					</ul>
-					<?php get_template_part( 'template-component/component-pagination', 'archive' ); ?>
-				<?php endif; ?>
+				
 			<?php endif; ?>
 			<?php do_action('wsu_wds_theme_after_header', 'search'); ?>
-			<?php get_template_part( 'template-component/component-search-block', 'search' ); ?>
 			<?php do_action('wsu_wds_theme_after_content', 'search'); ?>
 		</main>
 		<?php do_action('wsu_wds_theme_after_main', 'search'); ?>
