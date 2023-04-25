ce32d33526a73d8e6bbe1d250fdd58c53f02da73
Danial Bleile <danbleile@gmail.com>
2023-04-25 08:59:19 -0700

    Enhancement | Header Unit | Support subtitle link

--- theme-blocks/header-unit/theme-block.php
+++ theme-blocks/header-unit/theme-block.php
@@ -6,7 +6,8 @@ class Theme_Block_Header_Unit extends Theme_Block {
 	protected static $panel_type   = 'theme';
 	protected static $option_slug  = 'theme_block_header_unit';
 	protected static $default_args = array(
-		'give_link' => 'https://foundation.wsu.edu/give/?utm_source=wsu-pullman&utm_medium=wsu-link&utm_campaign=wsu-pullman-referral',
+		'give_link'    => 'https://foundation.wsu.edu/give/?utm_source=wsu-pullman&utm_medium=wsu-link&utm_campaign=wsu-pullman-referral',
+		'subtitleLink' => '',
 	);
 
 
@@ -33,11 +34,18 @@ class Theme_Block_Header_Unit extends Theme_Block {
 
 	protected static function parse_options( &$args, $context ) {
 
-		$give_link = WSU_Option::get( 'site_options', 'give_link', '' );
+		$give_link     = WSU_Option::get( 'site_options', 'give_link', '' );
+		$subtitle_link = WDS_Options::get( 'site', 'subtitle_link', '' );
 
 		if ( ! empty( $give_link ) ) {
 
 			$args['give_link'] = $give_link;
+
+		}
+
+		if ( ! empty( $subtitle_link ) ) {
+
+			$args['subtitleLink'] = $subtitle_link;
 
 		}
 
